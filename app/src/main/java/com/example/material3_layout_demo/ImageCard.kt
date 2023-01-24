9122a52097c1683bac74a1b8cb4bdbdb60c71d70
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-01-24 11:26:38 -0500

    CONFIGURE themes and colors

---
+++ app/src/main/java/com/example/material3_layout_demo/ImageCard.kt
@@ -0,0 +1,13 @@
+package com.example.material3_layout_demo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ImageCard(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+){
+
+}
---
+++ app/src/main/java/com/example/material3_layout_demo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.material3_layout_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.material3_layout_demo.ui.theme.Material3layout_demoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Material3layout_demoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+
