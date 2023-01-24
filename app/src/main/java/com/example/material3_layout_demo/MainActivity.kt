e202f20998aff9902f3d0f10cf03d38006299662
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-01-24 13:02:58 -0500

    ADDED Layout in main

--- app/src/main/java/com/example/material3_layout_demo/MainActivity.kt
+++ app/src/main/java/com/example/material3_layout_demo/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.material3_layout_demo.ui.theme.Material3layout_demoTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +24,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = { }) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer
+                                )
+                            }
+                        },
+                        topBar = {
+                            MediumTopAppBar(
+                                title = {
+                                    Text(text = "Material3 & Compose")
+                                },
+                                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+
+                        }
+                    ) 
+                    { values ->
+                       LazyColumn(contentPadding = values){
+                           items(count = 20) {
+                               ImageCard(
+                                   title = "loerm ipsum",
+                                   description = "lorem ipsum supem sjapa",
+                                   modifier = Modifier.padding(16.dp))
+                           }
+                       }
+                    }
                 }
             }
         }
